3ab749fdd3b3e06d221235358073e211aa23d609
balakinsergey83 <balakinsergey83@gmail.com>
2018-02-22 15:17:02 +0200

    DZ_5.3

--- DZ-5.3/Program.cs
+++ DZ-5.3/Program.cs
@@ -10,6 +10,26 @@ namespace DZ_5._3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Четные или нечетные между данными");
+            Console.WriteLine();
+            int a = int.Parse(Console.ReadLine());
+           
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for(int x = 0; a < b; a++)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.Write($"{a}  четные между числа  {a} и {b}         ");
+                }
+                else if (a % 2 != 0)
+                {
+                    Console.WriteLine($"       {a}  нечетные между числами  {a} и {b}");
+                }
+            
+            }
+            Console.ReadKey();
+            
         }
     }
 }
